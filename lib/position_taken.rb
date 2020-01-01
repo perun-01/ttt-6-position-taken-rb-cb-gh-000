beacc8865ef4c0f9973d6b8756fff349883b9530
Ranjeet Singh Sahni <singhranjeet5200@gmail.com>
2020-01-01 05:44:39 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,7 +3,7 @@ def position_taken?(board, index)
   if board[index]= " "
     return false
   elsif board[index]= "X" || board[index]= "O"
-    return false
+    return true
   else
     return false
   end
