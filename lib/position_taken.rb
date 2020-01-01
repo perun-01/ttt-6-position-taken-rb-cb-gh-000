954eceaffdafe5387b8de2e4f8953af1534e3def
Ranjeet Singh Sahni <singhranjeet5200@gmail.com>
2020-01-01 05:55:19 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,10 +1,10 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  if board[index]= " " || board[index]= ""
+  if board[index-1]= " " || board[index]= ""
     return false
-  elsif board[index]= nil
+  elsif board[index-1]= nil
     return false
-  elsif board[index]= "X" || board[index]= "O"
+  elsif board[index-1]= "X" || board[index]= "O"
     return true
   end
 end
